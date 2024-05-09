c336409c7eb57d54ddb5d872695213a8579f1294
JonnesLin <jonneslin@gmail.com>
2024-05-10 02:27:38 +0800

    commit

--- train2.py
+++ train2.py
@@ -11,17 +11,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.image as mpimg 
 from PIL import Image,ImageDraw
-from sklearn.decomposition import PCA
+import nvdiffrast.torch as dr
 import matplotlib
 import torchvision.transforms as T
-import nvdiffrast.torch as dr
 
-img_path='./outputs/_DEMO/ski/img/000001.jpg'
+
+img_path='./outputs/_DEMO/run/img/000001.jpg'
 file_path='./outputs/_PKL/ski/smpl.pkl'
+device = torch.device('cuda:1')
 patch_size = 14
 feat_dim= 384
-dinov2_vits14 = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14')
-glctx = dr.RasterizeGLContext(device='cuda:0')
+dinov2_vits14 = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14').to(device)
 img = Image.open(img_path).convert('RGB')
 H,W=img.height, img.width
 max_dimension = max(W, H)
@@ -33,12 +33,13 @@ transform = T.Compose([
     T.ToTensor(),
     T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
 ])
-img_tensor = transform(img)[:3].unsqueeze(0).to('cuda')
+
 features = torch.zeros(1, patch_H * patch_W, feat_dim)
-imgs_tensor = torch.zeros(1, 3, patch_H * patch_size, patch_W * patch_size)
-imgs_tensor[0] = transform(img)[:3]
-with torch.no_grad():   
+imgs_tensor = torch.zeros(1, 3, patch_H * patch_size, patch_W * patch_size,device=device)
+imgs_tensor[0] = transform(img)[:3].unsqueeze(0).to(device)
+with torch.no_grad():     
     features_dict = dinov2_vits14.forward_features(imgs_tensor)
+
     features = features_dict['x_norm_patchtokens']
 features=features.reshape(1,feat_dim,patch_H,patch_W).to('cuda')
 
@@ -48,7 +49,7 @@ with open(file_path, 'rb') as file:
 with open('./SMPL_NEUTRAL.pkl', 'rb') as file:
     smpl_data = pickle.load(file, encoding='latin1')
 faces=smpl_data['f'] #13776,3
-faces = faces.astype(np.float32)
+faces = faces.astype(np.float16)
 faces=torch.tensor(faces).to('cuda')
 frame0=data[0]
 _2d_vertices_0=torch.tensor(frame0['2d_vertices'][0]).to('cuda') #[0,1]
@@ -62,8 +63,8 @@ point_features = torch.nn.functional.grid_sample(   #torch.Size([1, 384, 6890, 1
     mode='bilinear',  # 可以使用 'nearest' 或 'bicubic'
     padding_mode='border',  # 可以使用 'zeros' 或 'reflection'
     align_corners=True
-).squeeze(3)
-render_image=torch.tensor(len(data),)
+).squeeze(3).to(device)
+render_image=torch.tensor(len(data))
 # Process each frame's data
 for frame in data:
     frame_index = frame['frame_index'][0]
@@ -85,11 +86,11 @@ for frame in data:
             pose2rot=True
         )
     vertices=output.vertices.clone()
-    homogeneous_vertices = torch.cat([vertices, torch.ones_like(vertices[:, :, :1])], dim=-1)
-    faces=faces.clone().to(torch.int32)
-    rast_out, rast_out_db = dr.rasterize(glctx, homogeneous_vertices, faces, resolution=[newH, newW])
-    rast_out=rast_out.to('cuda')
-
+    homogeneous_vertices = torch.cat([vertices, torch.ones_like(vertices[:, :, :1])], dim=-1).to(device)
+    faces=faces.clone().to(torch.int32).to(device)
+    glctx = dr.RasterizeCudaContext(device=device)
+    rast_out, rast_out_db = dr.rasterize(glctx, homogeneous_vertices, faces, resolution=[newH-newH%8, newW-newW%8])
+    rast_out=rast_out.to(device)
     interpolated_colors,_ = dr.interpolate(point_features, rast_out,faces)
     print(interpolated_colors)
     import ipdb;ipdb.set_trace()
@@ -108,12 +109,13 @@ for frame in data:
     offset = ((max_dimension - W) // 2, (max_dimension - H) // 2)
     _2d_vertices[:, 0] = (_2d_vertices[:, 0] * max(W,H)-offset[0])/W*newW 
     _2d_vertices[:, 1] = _2d_vertices[:, 1] * max(W,H)-offset[1]/H*newH   #[6890, 2]
-    # img = T.ToPILImage()(img_tensor.squeeze()).convert("RGB")
-    # draw = ImageDraw.Draw(img)
-    # for x, y in _2d_vertices:
-    #     # 在x, y位置画点，这里直接在张量上修改值
-    #     draw.ellipse((x - 3, y - 3, x + 3, y + 3), fill='red')
-    # img.save("./haha.png")
+    img = T.ToPILsImage()(img_tensor.squeeze()).convert("RGB")
+    draw = ImageDraw.Draw(img)
+    for x, y in _2d_vertices:
+        # 在x, y位置画点，这里直接在张量上修改值
+        draw.ellipse((x - 3, y - 3, x + 3, y + 3), fill='red')
+    img.save("./haha.png")
+    break
 
     
   
